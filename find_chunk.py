016db9bd251775ad5dc63b1673701ab6cb8a91e4
Anirudh Todi <anirudhtodi@berkeley.edu>
2011-10-21 21:24:01 -0700

    Implemented some gzip compression - haven't tested as yet

--- find_chunk.py
+++ find_chunk.py
@@ -1,14 +1,28 @@
- import glob
+from glob import glob
+from gzip import GzipFile
+from StringIO import StringIO
 
 class Find_Chunk():
     def find_chunk(file_name, start_byte_number, end_byte_number):
+        # Open file
         f = open(file_name, 'rb')
+        # Seek to the byte we want
         ignore_bytes = f.read(start_byte_number)
+        # Read the bytes to return
         bytes_to_read = f.read(end_byte_number - start_byte_number)
-        return bytes_to_read
+        # Write the bytes to StringIO
+        output = StringIO()
+        output.write(bytes_to_read)
+        # Compress the StringIO
+        gzf = GzipFile(fileobj=output, mode='wb')
+        gzf.write(output)
+        gzf.close
+        # Return the compressed StringIO
+        return gzf
     
     def find_file(file_name):
-        path = glob.glob(file_name)
+        # Check if the file_name exists in our current directory
+        path = glob(file_name)
         if path:
             return True
         else:
