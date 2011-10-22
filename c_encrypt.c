f0b665ce984014b49eeeaca70cf40299c76ef86d
Hoa Long Tam <hoalong.tam@berkeley.edu>
2011-10-22 02:56:58 -0700

    more progress on encryption

--- c_encrypt.c
+++ c_encrypt.c
@@ -1,25 +1,26 @@
-#include "Python.h"
+#include <Python.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <openssl/rsa.h>
 #include <openssl/engine.h>
 #include <openssl/err.h>
+#include <openssl/pem.h>
 
 /**
  * Encrypts string using the given public key.  Returns the number of blocks in
  * the buffer.  Places the blocks in BUF.  BUF should be large enough to hold
  * the entire string.
  */
-static int encrypt(char* string, RSA* keypair, char* buf) {
+static int encrypt_str(char* string, RSA* keypair, char* buf) {
   size_t len = strlen(string);
   int size = RSA_size(keypair);
   int blocks = (len - 1) / size + 1;
   int start = 0;
   int end = size - 11; // 11 bits for padding
   while (end <= len) {
-    int bytes = RSA_public_encrypt(end - start, string + start, buf, rsa,
-                                   RSA_PKCS1_PADDING);
+    int bytes = RSA_public_encrypt(end - start, (const unsigned char*)string + start,
+                                   (unsigned char*)buf, keypair, RSA_PKCS1_PADDING);
     start += bytes;
     end += bytes;
     if (end > len && start < len) { end = len; }
@@ -32,37 +33,48 @@ static int encrypt(char* string, RSA* keypair, char* buf) {
 /**
  * Encrypts a Python string.
  */
-static PyObject* py_encrypt(PyObject* string, RSA* keypair) {
-  if (PyString_Check(string)) {
-    char* str = PyString_AsString(string);
-    char* buf = malloc((strlen(str) - 1 / (RSA_size(keypair) - 11) + 1)
-                       * RSA_size(keypair));
-    int blocks = encrypt(str, keypair, buf);
-    PyObject* result = PyString_FromString(buf);
+PyObject* py_encrypt(PyObject* string, PyObject* fname) {
+  if (PyString_Check(string) && PyString_Check(fname)) {
+    /* First, read in public key. */
+    char* keyfile = PyString_AsString(fname);
+    RSA* keypair;
+    FILE* fin = fopen(keyfile, "r");
+    keypair = PEM_read_RSAPublicKey(fin, NULL, NULL, NULL);
+    fclose(fin);
+
+    unsigned char* str = (unsigned char*)PyString_AsString(string);
+    unsigned char* buf = (unsigned char*)malloc((strlen((char*)str) - 1 /
+                                                 (RSA_size(keypair) - 11) + 1)
+                                                * RSA_size(keypair));
+    int blocks = encrypt_str((char*)str, keypair, (char*)buf);
+    PyObject* result = PyString_FromString((char*)buf);
     return result;
   }
   return NULL;
 }
 
+static PyMethodDef methods[] = {
+  {"encrypt", py_encrypt, METH_VARARGS},
+  {NULL, NULL, 0, NULL}
+};
+
+static PyObject* EncryptError;
+
+PyMODINIT_FUNC initencrypt(void) {
+  PyObject* m;
+  m = Py_InitModule("c_encrypt", methods);
+  if (m == NULL) return;
+  EncryptError = PyErr_NewException("c_encrypt.error", NULL, NULL);
+  Py_INCREF(EncryptError);
+  PyModule_AddObject(m, "error", EncryptError);
+}
 
 
 int main(int argc, char** argv) {
-  RSA* rsa = RSA_generate_key(256, 17, NULL, NULL);
-  int size = RSA_size(rsa);
-  printf("I need at least %d bytes.\n", RSA_size(rsa));
-  char* input = malloc(size + 1);
-  strcpy(input, "Hello, world!");
-  char *output = malloc(size + 1);
-  char *result = malloc(size + 1);
-  input[size] = '\0';
-  output[size] = '\0';
-  result[size] = '\0';
-
-  printf("Encoding '%s'.\n", input);
-  int ret_size = RSA_public_encrypt(15, input, output, rsa, RSA_PKCS1_PADDING);
-  //printf("Encrypted %d bytes: '%s'.\n", ret_size, output);
-  int dec_bytes = RSA_private_decrypt(ret_size, output, result, rsa, RSA_PKCS1_PADDING);
-  printf("Decrypted %d bytes: '%s'.\n", dec_bytes, result);
+  RSA* rsa = RSA_generate_key(512, 17, NULL, NULL);
+  FILE* fout = fopen("test.pem", "w");
+  PEM_write_RSA_PUBKEY(fout, rsa);
+  fclose(fout);
   RSA_free(rsa);
   return 0;
 }
