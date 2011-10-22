093acd4fdc62b5204a8856a4e19dd12d92f5c28e
Anirudh Todi <anirudhtodi@berkeley.edu>
2011-10-21 22:08:08 -0700

    Ditched compression. Passing plain strings across the wire

--- find_chunk.py
+++ find_chunk.py
@@ -2,29 +2,53 @@ from glob import glob
 from gzip import GzipFile
 from StringIO import StringIO
 
-class Find_Chunk():
-    def find_chunk(file_name, start_byte_number, end_byte_number):
+class Find_Chunk:
+    def find_chunk(self, file_name, start_byte_number, end_byte_number):
         # Open file
         f = open(file_name, 'rb')
         # Seek to the byte we want
         ignore_bytes = f.read(start_byte_number)
         # Read the bytes to return
         bytes_to_read = f.read(end_byte_number - start_byte_number)
-        # Write the bytes to StringIO
-        output = StringIO()
-        output.write(bytes_to_read)
-        # Compress the StringIO
-        gzf = GzipFile(fileobj=output, mode='wb')
-        gzf.write(output)
-        gzf.close
-        # Return the compressed StringIO
-        return gzf
+        print bytes_to_read
+        return bytes_to_read
+        # # Write the bytes to StringIO
+        # output = StringIO()
+        # output.write(bytes_to_read)
+        # print output.getvalue()
+        # # Compress the StringIO
+        # gzf = GzipFile(fileobj=output, mode='ab')
+        # gzf.write(output.getvalue())
+        # print gzf.read()
+        # gzf.close
+        # # Return the compressed StringIO
+        # return gzf
     
-    def find_file(file_name):
+    def uncompress_chunk(self, compressed_chunk):
+        print compressed_chunk
+    
+        # print compressed_chunk.read()
+        # sio = StringIO()
+        # sio.write(compressed_chunk)
+        # print sio.getvalue()
+
+        # gzf = GzipFile(fileobj=sio, mode='wb')
+        # gzf.write(sio.getvalue())
+        # print gzf.read()
+        # gzstream = open(sio.getvalue(), 'rb')
+        # sio = StringIO(gzstream)
+        # gz = GzipFile(fileobj=sio, mode='rb')
+        # print gz.read()
+    
+    def find_file(self, file_name):
         # Check if the file_name exists in our current directory
         path = glob(file_name)
         if path:
             return True
         else:
             return False
-             
+
+a = Find_Chunk()
+compressed_chunk = a.find_chunk('test.txt', 1, 3)
+a.uncompress_chunk(compressed_chunk)
+        
