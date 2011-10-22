6e2ecda8322d5cac5e96ab98f62e469419ec1322
Hoa Long Tam <hoalong.tam@berkeley.edu>
2011-10-22 04:47:54 -0700

    more encryption

---
+++ encryption.py
@@ -0,0 +1,50 @@
+from subprocess import Popen, PIPE
+
+def make_key():
+    p = Popen(['openssl', 'genrsa', '-out', 'key.pem', '2048'])
+    p.communicate()
+    Popen(['openssl', 'rsa', '-in', 'key.pem', '-pubout', '-out',
+           'pub-key.pem'])
+
+def encrypt(data):
+    start = 0
+    block_sz = 244
+    result = []
+    while start < len(data) - 1:
+        block = data[start:start + block_sz]
+        p = Popen(['openssl', 'rsautl', '-encrypt', '-inkey',
+                   'pub-key.pem', '-pubin'], stdin=PIPE, stdout=PIPE)
+        out, err = p.communicate(block)
+        result.append(out)
+        start += block_sz
+    return ''.join(result)
+
+def decrypt(data):
+    start = 0
+    block_sz = 256
+    result = []
+    while start < len(data) - 1:
+        block = data[start: start + block_sz]
+        p = Popen(['openssl', 'rsautl', '-decrypt', '-inkey',
+                   'key.pem'], stdin=PIPE, stdout=PIPE)
+        out, err = p.communicate(block)
+        result.append(out)
+        start += block_sz
+    return ''.join(result)
+
+def rand_string():
+    import random
+    import string
+    s = ""
+    for i in xrange(1, random.randrange(1, 5)):
+        s += random.choice(string.lowercase)
+    s *= random.randint(2, 124)
+    return s
+
+
+if __name__ == "__main__":
+    for i in xrange(1000):
+        s = rand_string()
+        e = encrypt(s)
+        r = decrypt(e)
+        assert s != e and e != r and s == e
