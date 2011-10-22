3df1c4f8056463ccff0d76a2e555d5c16538dfcb
Lyle Mills <lylepmills@gmail.com>
2011-10-22 03:23:44 -0700

    compression removed for now

--- filemanager.py
+++ filemanager.py
@@ -12,10 +12,12 @@ class FileManager:
         f = open(file_name, 'rb')
         ignore_bytes = f.read(start_byte_number)
         bytes_to_read = f.read(end_byte_number - start_byte_number)
-        return zlib.compress(binascii.b2a_uu(bytes_to_read))
+        return bytes_to_read
+#        return zlib.compress(binascii.b2a_uu(bytes_to_read))
     
     def uncompress_chunk(self, compressed_chunk):
-        return binascii.a2b_uu(zlib.decompress(compressed_chunk))
+        return compressed_chunk
+ #       return binascii.a2b_uu(zlib.decompress(compressed_chunk))
     
     def find_file(self, file_name):
         # Check if the file_name exists in our current directory
